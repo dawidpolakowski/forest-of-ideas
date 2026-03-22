0e39c934f920dbe58c48567a353d9dfbd4919e0f
dawidpolakowski <dawidpolakowski@gmail.com>
2026-03-22 22:50:00 +0000

    initial portal _ mtg arena page

---
+++ data.js
@@ -0,0 +1,8 @@
+const ideas = [
+    {
+        title: "MTG Arena – Deck Simulation Mode",
+        file: "ideas/mtg-arena-improvements.html",
+        category: "Games",
+        tags: ["mtg", "deckbuilding", "simulation"]
+    }
+];
